bbb3aa058b692645a0cfc448687f987773597c4d
James <jamesade646@gmail.com>
2021-05-18 00:13:37 -0400

    update3

--- app/src/main/java/com/example/betterwayass/MainActivity.kt
+++ app/src/main/java/com/example/betterwayass/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.betterwayass
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import android.widget.Button
+import android.app.ActionBar.LayoutParams
+import androidx.appcompat.app.ActionBar
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.betterwayass.adapter.MainAdapter
 import com.example.betterwayass.databinding.ActivityMainBinding
 import com.example.betterwayass.model.Category
 import com.example.betterwayass.model.Contact
+import kotlinx.android.synthetic.main.contact_view.view.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,11 +25,15 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        title= "Categories"
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setup(binding)
+
 
     }
 
@@ -32,7 +41,6 @@ class MainActivity : AppCompatActivity() {
         val recyclerView=binding.contactCategory
         val layoutManager = GridLayoutManager(this,2,GridLayoutManager.VERTICAL,false)
         recyclerView.layoutManager = layoutManager
-        
         recyclerView.adapter = adapter
 
     }
